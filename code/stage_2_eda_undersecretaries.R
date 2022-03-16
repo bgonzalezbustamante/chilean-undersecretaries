58ac75fed1d74e93993cf61b837f83389fbd8153
Bastián González-Bustamante <bastian.gonzalezbustamante@politics.ox.ac.uk>
2022-03-16 13:42:13 -0300

    Create stage_2_eda_undersecretaries.R

---
+++ code/stage_2_eda_undersecretaries.R
@@ -0,0 +1,51 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#### Script ID ####
+
+## Exploratory Data Analysis Undersecretaries
+## R version 4.1.0 (2021-05-18) -- "Camp Pontanezen"
+## Date: November 2021
+
+## Bastián González-Bustamante (University of Oxford, UK)
+## https://bgonzalezbustamante.com
+## Alejandro Olivares (Universidad Católica de Temuco, Chile)
+
+## Data Set on Chilean Undersecretaries
+## https://github.com/bgonzalezbustamante/chilean-undersecretaries
+## https://doi.org/10.5281/zenodo.5715384
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#### Packages and Data ####
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## Clean Environment
+rm(list = ls())
+
+## Packages
+library(DataExplorer)
+library(ggplot2)
+
+## Data
+data_CHL <- read.csv("data/tidy/chl_undersecretaries.csv", encoding = "UTF-8")
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#### EDA ####
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## Data Set Plot 
+plot_intro(data_CHL, ggtheme = theme_minimal(base_size = 12),
+           theme_config = theme(legend.position = "none")) 
+
+## Discrete Variables
+selection_var1 <- drop_columns(data_CHL, c("id", "country", "name", "start_president", "end_president",
+                                           "ministry", "undersec", "start_undersec", "end_undersec"))
+plot_bar(selection_var1, ggtheme = theme_minimal(base_size = 12))
+
+## Time Variable
+data_CHL$time <- with(data_CHL, (as.Date(data_CHL$end_undersec) - as.Date(data_CHL$start_undersec)))
+data_CHL$time <- as.numeric(data_CHL$time)
+plot_histogram(data_CHL$time, ggtheme = theme_minimal(base_size = 12))
